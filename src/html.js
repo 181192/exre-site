5d02b1ea932d668b55c822860bb9f28dd90bac5a
181192 <181192@stud.hvl.no>
2018-11-28 18:47:03 +0100

    Add html layout

---
+++ src/html.js
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import ogImage from "../static/default-ogimage.jpg";
+import favicon from "../static/favicon.ico";
+
+export default class HTML extends React.Component {
+  render() {
+    return (
+      <html {...this.props.htmlAttributes} lang="en">
+        <Helmet
+          title={`ExRe - Exam Resources 2018`}
+          meta={[
+            {
+              name: "description",
+              content: `This page is build with Gatsby 💜 (a React based static site generator) and is hosted on Github Pages.`
+            },
+            { property: "og:type", content: "website" },
+            {
+              property: "og:url",
+              content: `https://181192.github.io/exre-site/`
+            },
+            { property: "og:image", content: `${ogImage}` },
+            { property: "og:title", content: `ExRe | Exam Resources 2018` },
+            {
+              property: "og:description",
+              content: `This page is build with Gatsby 💜 (a React based static site generator) and is hosted on Github Pages.`
+            }
+          ]}
+          link={[
+            { rel: "shortcut icon", type: "image/png", href: `${favicon}` }
+          ]}
+        />
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          {this.props.headComponents}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    );
+  }
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array
+};
